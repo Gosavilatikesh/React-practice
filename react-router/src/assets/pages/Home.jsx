f367f430b237aea3e84e19948dc121d13b60a9f2
Gosavilatikesh <gosavilatikesh18@gmail.com>
2026-07-18 01:14:58 +0530

    "commit"

--- react-router/src/assets/pages/Home.jsx
+++ react-router/src/assets/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Outlet, useNavigate } from 'react-router'
 
 const Home = () => {
+
+    let navigate = useNavigate()
+
   return (
     <div>
       <h1>home page</h1>
+      <button onClick={() => navigate('/home/detail')}>detail show</button>
+      <Outlet />
     </div>
   )
 }
